d1afea66627c13f93770e5650cea0a5b2851e977
Mac User <alexarobinson19@gmail.com>
2017-11-24 00:23:25 -0500

    Added trivia questions. They are in an object called triviaQs and then within an array of questions. within the Questions array there are arrays that hold two arrays, one that includes the question and answer and another that holds the different choices that will display.

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+var triviaQs={
+    Questions:[[["Where is the Doctor from?", "Gallifrey"], ["Gallifrey", "Earth", "Mars", "New Earth"]],
+        [["Who was the most recent doctor?", "Peter Capaldi"], ["David Tenant", "Matt Smith", "Peter Capaldi", "William Hartnell"]],
+        [["Who will be the next doctor?", "Jodie Whittaker"], ["David Tenant", "Ian McKellan", "Iris O'Bannon", "Jodie Whittaker"]],
+        [["Who was the Tenth Doctor?", "David Tenant"], ["David Tenant", "Matt Smith", "Christopher Eccleston", "Patrick Stewart"]],
+        [["When did the Doctor meet River Song for the first time?", "Silence in the Library"], ["Rose", "Blink", "Silence in the Library", "Let's Kill Hitler"]],
+        [["Who are River Song's parents?", "Amelia and Rory Pond"], ["Amelia and Rory Pond", "The Doctor and Romana", "The Doctor and Sarah Jane Smith", "We don't know"]],
+        [["What is the band that writes and performs songs about the Doctor?", "Chameleon Circuit"], ["Blink-182", "Chameleon Circuit", "Green Day", "Weird Sisters"]],
+        [["Who is 'the Doctor's Wife'?", "The TARDIS"], ["River Song", "The TARDIS", "Rose Tyler", "Donna Noble"]],
+        [["What does TARDIS stand for?", "Time and Relative Dimensions in Space"], ["Time and Raging Daleks in Space", "Time and Relative Dimensions in Space", "Theories and Returned Deposits in Sofas", "Time and Raunchy Dimensions in Scenes"]],
+        [["What species fights the Time Lords in the Time War?", "The Daleks"], ["The Daleks", "The Cybermen", "Humans", "The Cat Nurses"]],
+        [["Who is the Doctor's nemesis?", "The Master"], ["Romana", "Rose Tyler", "The Master", "Donna Noble"]],
+        [["Who does Rose Tyler bring back from the dead?", "Capt. Jack Harkness"], ["The Doctor", "The Master", "her mother", "Capt. Jack Harkness"]],
+        [["Which comedian was a companion of the Doctor for a season?", "Catherine Tate"], ["Billy Piper", "Robin Williams", "Jimmy Savile", "Catherine Tate"]],
+        [["What spin-off featured John Barrow as leader, Captain Jack Harkness?", "Torchwood"], ["Doctor Companions in Space", "Queen Victoria's Revenge", "Torchwood", "Teachingwood"]],
+        [["who started Torchwood?", "Queen Victoria"], ["The Doctor", "Queen Victoria", "Rose Tyler", "Capt. Jack Harkness"]],
+        [["What kid spin-off show ended with the death of its titular character?", "The Sarah Jane Smith Chronicles"], ["Torchwood", "Doctor Companions in Space", "The Sarah Jane Smith Chronicles", "Doctor Who"]],
+        [["What was the name of the Doctor's robot dog?", "K-9"], ["Spot", "K-9", "Kellog", "Super Dog"]],
+        [["What actor played the first Doctor?", "William Hartnell"], ["William Hartnell", "Tom Baker", "Matt Smith", "John Barrow"]],
+        [["What disease prevented William Hartnell from continuing his role as the Doctor?", "Multiple Sclerosis"], ["Syphilis", "Multiple Sclerosis", "Lupus", "cancer"]],
+        [["Which actor played the Doctor weaering an outrageously long scarf?", "Tom Baker"], ["William Hartnell", "Jodie Whittaker", "Tom Baker", "Matt Smith"]],
+
+]
+}
